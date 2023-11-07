fcff0c477485888c8a91a3f3e1188e8efdfc7de9
masor <masor.dev@gmail.com>
2023-11-07 22:15:54 +0300

    lesson 2.2 done

--- app/src/main/java/com/masorone/jetpackcompose/MainActivity.kt
+++ app/src/main/java/com/masorone/jetpackcompose/MainActivity.kt
@@ -3,7 +3,11 @@ package com.masorone.jetpackcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import com.masorone.jetpackcompose.lesson.l_2_1.UserInfo
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import com.masorone.jetpackcompose.lesson.l_2_2.UserInfo
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,4 +16,10 @@ class MainActivity : ComponentActivity() {
             UserInfo(name = "Fedor", age = 24)
         }
     }
+}
+
+@Composable
+@Preview
+fun Greeting() {
+    Text(text = "Hello", color = Color.Blue)
 }
