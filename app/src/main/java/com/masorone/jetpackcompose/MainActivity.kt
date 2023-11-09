13eb5c8548d95f0b1bbd52532ec887d1305e4f49
masor <masor.dev@gmail.com>
2023-11-09 22:16:54 +0300

    lesson 2.5 done

--- app/src/main/java/com/masorone/jetpackcompose/MainActivity.kt
+++ app/src/main/java/com/masorone/jetpackcompose/MainActivity.kt
@@ -3,7 +3,7 @@ package com.masorone.jetpackcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import com.masorone.jetpackcompose.lesson.l_2_4.InstagramProfileCard
+import com.masorone.jetpackcompose.lesson.l_2_5.InstagramProfileCard
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
