97c73a65e8156f6a2b767b55f39e39ee2c91de05
masor <masor.dev@gmail.com>
2023-11-11 23:24:42 +0300

    lesson 2.10 done

---
+++ app/src/main/java/com/masorone/jetpackcompose/lesson/l_2_10/InstagramProfileCard.kt
@@ -0,0 +1,157 @@
+package com.masorone.jetpackcompose.lesson.l_2_10
+
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.masorone.jetpackcompose.R
+import com.masorone.jetpackcompose.ui.theme.JetpackComposeTheme
+
+@Composable
+fun InstagramProfileCard() {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .wrapContentHeight()
+            .padding(8.dp),
+        border = BorderStroke(1.dp, MaterialTheme.colorScheme.onBackground),
+        shape = RoundedCornerShape(16.dp),
+        colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.background
+        ),
+        elevation = CardDefaults.cardElevation(
+            defaultElevation = 8.dp
+        )
+    ) {
+        AccountTopContent()
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun InstagramProfileCardLight() {
+    JetpackComposeTheme(
+        darkTheme = false
+    ) {
+        InstagramProfileCard()
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun InstagramProfileCardDark() {
+    JetpackComposeTheme(
+        darkTheme = true
+    ) {
+        InstagramProfileCard()
+    }
+}
+
+@Composable
+private fun AccountTopContent() {
+    Column {
+        Row(
+            Modifier
+                .fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceEvenly,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            AccountIcon()
+            UserStatistics(title = "Posts", value = "6,950")
+            UserStatistics(title = "Followers", value = "105M")
+            UserStatistics(title = "Following", value = "650")
+        }
+        Text(
+            text = "Instagram",
+            modifier = Modifier
+                .padding(start = 16.dp),
+            fontFamily = FontFamily.Cursive,
+            fontSize = 28.sp
+        )
+        Text(
+            text = "#YorsToMake",
+            modifier = Modifier
+                .padding(start = 16.dp),
+            fontSize = 14.sp
+        )
+        Text(
+            text = "www.facebook.com/emotionalhealth",
+            modifier = Modifier
+                .padding(start = 16.dp),
+            fontSize = 14.sp
+        )
+        Button(
+            onClick = { },
+            shape = RoundedCornerShape(16.dp),
+            colors = ButtonDefaults.buttonColors(
+                containerColor = MaterialTheme.colorScheme.primary
+            ),
+            modifier = Modifier
+                .padding(start = 16.dp, bottom = 16.dp)
+                .clickable { },
+        ) {
+            Text(text = "Follow")
+        }
+    }
+}
+
+@Composable
+private fun AccountIcon() {
+    Image(
+        painter = painterResource(id = R.drawable.ic_instagram),
+        modifier = Modifier
+            .clip(CircleShape)
+            .background(Color.White),
+        contentDescription = ""
+    )
+}
+
+@Composable
+private fun UserStatistics(
+    title: String,
+    value: String
+) {
+    Column(
+        Modifier
+            .height(80.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.SpaceEvenly
+    ) {
+        Text(
+            text = value,
+            fontFamily = FontFamily.Cursive,
+            fontSize = 24.sp,
+            fontWeight = FontWeight.Bold
+        )
+        Text(
+            text = title,
+            fontWeight = FontWeight.Bold
+        )
+    }
+}
