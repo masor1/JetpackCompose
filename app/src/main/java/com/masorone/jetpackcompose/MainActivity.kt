bbc22b33cb94ffa790100639f21b05126f952cd1
masor <masor.dev@gmail.com>
2023-11-11 23:06:04 +0300

    lesson 2.9 done

--- app/src/main/java/com/masorone/jetpackcompose/MainActivity.kt
+++ app/src/main/java/com/masorone/jetpackcompose/MainActivity.kt
@@ -8,7 +8,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.ui.Modifier
-import com.masorone.jetpackcompose.lesson.l_2_8.InstagramProfileCard
+import com.masorone.jetpackcompose.lesson.l_2_9.InstagramProfileCard
 import com.masorone.jetpackcompose.ui.theme.JetpackComposeTheme
 
 class MainActivity : ComponentActivity() {
