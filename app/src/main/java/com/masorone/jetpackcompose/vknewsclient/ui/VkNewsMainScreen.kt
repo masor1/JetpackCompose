e47103351180a0e344efb2de745f8158c343fa15
masor <masor.dev@gmail.com>
2024-01-06 23:38:52 +0300

    lesson 3.4 - FAB, snackbar done

--- app/src/main/java/com/masorone/jetpackcompose/vknewsclient/ui/VkNewsMainScreen.kt
+++ app/src/main/java/com/masorone/jetpackcompose/vknewsclient/ui/VkNewsMainScreen.kt
@@ -1,17 +1,30 @@
 package com.masorone.jetpackcompose.vknewsclient.ui
 
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarDuration
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.SnackbarResult
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import com.masorone.jetpackcompose.ui.CustomPreview
 import com.masorone.jetpackcompose.ui.theme.JetpackComposeTheme
+import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun VkNewsMainScreen() {
+    val hostState = SnackbarHostState()
+    val fabState = remember { mutableStateOf(true) }
+    val scope = rememberCoroutineScope()
     Scaffold(
         bottomBar = {
             NavigationBar {
@@ -27,7 +40,28 @@ fun VkNewsMainScreen() {
                     }
                 }
             }
-        }
+        },
+        floatingActionButton = {
+            if (fabState.value) {
+                FloatingActionButton(
+                    onClick = {
+                        scope.launch {
+                            val snackbarResult = hostState.showSnackbar(
+                                message = "This is Snackbar",
+                                actionLabel = "Hide FAB",
+                                duration = SnackbarDuration.Long
+                            )
+                            if (snackbarResult == SnackbarResult.ActionPerformed) {
+                                fabState.value = false
+                            }
+                        }
+                    }
+                ) {
+                    Icon(imageVector = Icons.Filled.Favorite, contentDescription = null)
+                }
+            }
+        },
+        snackbarHost = { SnackbarHost(hostState = hostState) }
     ) {
         it
     }
