784edba47361597116ed3cd78cc3f3d2ed6b9c12
masor <masor.dev@gmail.com>
2024-01-25 01:07:37 +0300

    10.1 - canvas (line, circle, rect)

--- custom/src/main/java/com/masorone/custom/MainCustomActivity.kt
+++ custom/src/main/java/com/masorone/custom/MainCustomActivity.kt
@@ -3,13 +3,7 @@ package com.masorone.custom
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import com.masorone.custom.sample.CanvasTest
 import com.masorone.custom.ui.theme.JetpackComposeTheme
 
 class MainCustomActivity : ComponentActivity() {
@@ -17,7 +11,7 @@ class MainCustomActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             JetpackComposeTheme {
-
+                CanvasTest()
             }
         }
     }
---
+++ custom/src/main/java/com/masorone/custom/sample/CanvasTest.kt
@@ -0,0 +1,126 @@
+package com.masorone.custom.sample
+
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.drawscope.DrawScope
+import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun CanvasTest() {
+    Canvas(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.Black)
+    ) {
+        drawLine(
+            color = Color.White,
+            start = Offset(0f, 0f),
+            end = Offset(size.width, size.height),
+        )
+        drawLine(
+            color = Color.White,
+            start = Offset(size.width, 0f),
+            end = Offset(0f, size.height),
+        )
+        drawCircle(
+            color = Color.Red,
+            radius = 100.dp.toPx(),
+            style = Stroke(2.dp.toPx())
+        )
+        fedor()
+    }
+}
+
+fun DrawScope.fedor() {
+    drawCircle(
+        color = Color.Cyan,
+        radius = 20.dp.toPx(),
+        center = Offset(70.dp.toPx(), 50.dp.toPx()),
+        style = Stroke(2.dp.toPx())
+    )
+    drawLine(
+        color = Color.Cyan,
+        start = Offset(70.dp.toPx(), 30.dp.toPx()),
+        end = Offset(70.dp.toPx(), 90.dp.toPx()),
+        strokeWidth = 2.dp.toPx()
+    )
+    drawLine(
+        color = Color.Cyan,
+        start = Offset(100.dp.toPx(), 30.dp.toPx()),
+        end = Offset(100.dp.toPx(), 90.dp.toPx()),
+        strokeWidth = 2.dp.toPx()
+    )
+    drawLine(
+        color = Color.Cyan,
+        start = Offset(100.dp.toPx(), 30.dp.toPx()),
+        end = Offset(120.dp.toPx(), 30.dp.toPx()),
+        strokeWidth = 2.dp.toPx()
+    )
+    drawLine(
+        color = Color.Cyan,
+        start = Offset(100.dp.toPx(), 70.dp.toPx()),
+        end = Offset(120.dp.toPx(), 70.dp.toPx()),
+        strokeWidth = 2.dp.toPx()
+    )
+    drawLine(
+        color = Color.Cyan,
+        start = Offset(100.dp.toPx(), 90.dp.toPx()),
+        end = Offset(120.dp.toPx(), 90.dp.toPx()),
+        strokeWidth = 2.dp.toPx()
+    )
+    drawLine(
+        color = Color.Cyan,
+        start = Offset(130.dp.toPx(), 90.dp.toPx()),
+        end = Offset(170.dp.toPx(), 90.dp.toPx()),
+        strokeWidth = 2.dp.toPx()
+    )
+    drawLine(
+        color = Color.Cyan,
+        start = Offset(150.dp.toPx(), 30.dp.toPx()),
+        end = Offset(140.dp.toPx(), 90.dp.toPx()),
+        strokeWidth = 2.dp.toPx()
+    )
+    drawLine(
+        color = Color.Cyan,
+        start = Offset(150.dp.toPx(), 30.dp.toPx()),
+        end = Offset(160.dp.toPx(), 90.dp.toPx()),
+        strokeWidth = 2.dp.toPx()
+    )
+    drawRect(
+        color = Color.Cyan,
+        topLeft = Offset(180.dp.toPx(), 30.dp.toPx()),
+        size = Size(20.dp.toPx(), 60.dp.toPx()),
+        style = Stroke(2.dp.toPx())
+    )
+    drawLine(
+        color = Color.Cyan,
+        start = Offset(210.dp.toPx(), 30.dp.toPx()),
+        end = Offset(210.dp.toPx(), 90.dp.toPx()),
+        strokeWidth = 2.dp.toPx()
+    )
+    drawLine(
+        color = Color.Cyan,
+        start = Offset(210.dp.toPx(), 30.dp.toPx()),
+        end = Offset(230.dp.toPx(), 30.dp.toPx()),
+        strokeWidth = 2.dp.toPx()
+    )
+    drawLine(
+        color = Color.Cyan,
+        start = Offset(230.dp.toPx(), 30.dp.toPx()),
+        end = Offset(230.dp.toPx(), 70.dp.toPx()),
+        strokeWidth = 2.dp.toPx()
+    )
+    drawLine(
+        color = Color.Cyan,
+        start = Offset(230.dp.toPx(), 70.dp.toPx()),
+        end = Offset(210.dp.toPx(), 70.dp.toPx()),
+        strokeWidth = 2.dp.toPx()
+    )
+}
