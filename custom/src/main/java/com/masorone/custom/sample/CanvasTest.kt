fc96d7441d020a11c4ab3cd60619cf1fd3e8ede8
masor <masor.dev@gmail.com>
2024-01-25 17:10:34 +0300

    10.2 - canvas (path)

--- custom/src/main/java/com/masorone/custom/sample/CanvasTest.kt
+++ custom/src/main/java/com/masorone/custom/sample/CanvasTest.kt
@@ -7,9 +7,14 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Path
+import androidx.compose.ui.graphics.TileMode
 import androidx.compose.ui.graphics.drawscope.DrawScope
+import androidx.compose.ui.graphics.drawscope.Fill
 import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
@@ -19,10 +24,37 @@ fun CanvasTest() {
     Canvas(
         modifier = Modifier
             .fillMaxSize()
-            .background(Color.Black)
+            .background(
+                brush = Brush.linearGradient(
+                    colors = listOf(Color.Cyan, Color.Magenta),
+                    end = Offset(20.toDpToPx(), 20.toDpToPx()),
+                    tileMode = TileMode.Mirror
+                )
+            )
     ) {
-        fedor()
+        drawPath(
+            path = Path().apply {
+                moveTo(center.x, 100.dp.toPx())
+                lineTo(center.x + 25.dp.toPx(), 150.dp.toPx())
+                lineTo(center.x + 75.dp.toPx(), 150.dp.toPx())
+                lineTo(center.x + 45.dp.toPx(), 195.dp.toPx())
+                lineTo(center.x + 60.dp.toPx(), 250.dp.toPx())
+                lineTo(center.x, 220.dp.toPx())
+                lineTo(center.x - 60.dp.toPx(), 250.dp.toPx())
+                lineTo(center.x - 45.dp.toPx(), 195.dp.toPx())
+                lineTo(center.x - 75.dp.toPx(), 150.dp.toPx())
+                lineTo(center.x - 25.dp.toPx(), 150.dp.toPx())
+                lineTo(center.x, 100.dp.toPx())
+            },
+            color = Color.Cyan,
+            style = Fill
+        )
     }
+}
+
+@Composable
+fun Int.toDpToPx() = with(LocalDensity.current) {
+    this@toDpToPx.dp.toPx()
 }
 
 fun DrawScope.fedor() {
