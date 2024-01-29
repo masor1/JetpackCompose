6538e3aaca0b587f80a75cff0f0f8df29438826d
masor <masor.dev@gmail.com>
2024-01-29 21:01:22 +0300

    VK API - 7.1 - Authorization

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -62,6 +62,8 @@ dependencies {
     implementation("androidx.compose.runtime:runtime-livedata:1.5.4")
     implementation("androidx.navigation:navigation-compose:2.7.6")
     implementation("com.google.code.gson:gson:2.10")
+    implementation("com.vk:android-sdk-core:4.1.0")
+    implementation("com.vk:android-sdk-api:4.1.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- app/src/main/java/com/masorone/jetpackcompose/vknewsclient/ui/MainActivity.kt
+++ app/src/main/java/com/masorone/jetpackcompose/vknewsclient/ui/MainActivity.kt
@@ -1,15 +1,37 @@
 package com.masorone.jetpackcompose.vknewsclient.ui
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
+import androidx.compose.runtime.SideEffect
 import com.masorone.jetpackcompose.ui.theme.JetpackComposeTheme
+import com.vk.api.sdk.VK
+import com.vk.api.sdk.auth.VKAuthenticationResult
+import com.vk.api.sdk.auth.VKScope
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             JetpackComposeTheme {
+                val authLauncher = rememberLauncherForActivityResult(
+                    contract = VK.getVKAuthActivityResultContract()
+                ) { result ->
+                    when (result) {
+                        is VKAuthenticationResult.Success -> {
+                            Log.d("MainActivity", "Success")
+                        }
+
+                        is VKAuthenticationResult.Failed -> {
+                            Log.d("MainActivity", "Failed")
+                        }
+                    }
+                }
+                SideEffect {
+                    authLauncher.launch(listOf(VKScope.WALL))
+                }
                 MainScreen()
             }
         }
